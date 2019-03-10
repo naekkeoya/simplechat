64ecb9111bfe87611895b55388364a049d7aafe7
Dany <danie.hdez03@gmail.com>
2019-03-09 22:01:34 -0600

    changed some things

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var PORT = process.env.PORT || 4000;
 var express = require ('express');
 var socket = require ('socket.io');
 
 // App setup
 
 var app = express();
-var server = app.listen(4000, function(){
+var server = app.listen(PORT, function(){
   console.log('listening to request on poort 4000');
 });
 
